c398aa9789e5192382ddd3e7398e8fae94d2821b
Paulo <github.pa12@gmail.com>
2025-04-02 13:11:23 -0300

    Correção: Ajuste na conexão com MySQL, desativando SSL

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,11 +1,4 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
-
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
